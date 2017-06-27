7b2bddeeb95032b8c919dde94032829625bd220b
constaac <aconstantine719@gmail.com>
2017-06-27 19:23:09 -0400

    Removes routes for Examples and Users(index/show)
    Comments out resources in routes for examples
    
    Comments out resources for users (index/show)
    
    Comments out index and show functions in users controller
    
    closes #17

--- config/routes.js
+++ config/routes.js
@@ -8,14 +8,13 @@ module.exports = require('lib/wiring/routes')
 .root('root#root')
 
 // standards RESTful routes
-.resources('examples')
+// .resources('examples')
 
 // users of the app have special requirements
 .post('/sign-up', 'users#signup')
 .post('/sign-in', 'users#signin')
 .delete('/sign-out/:id', 'users#signout')
 .patch('/change-password/:id', 'users#changepw')
-.resources('users', { only: ['index', 'show'] })
+// .resources('users', { only: ['index', 'show'] })
 
 // all routes created
-
