74e03cc983bda1186fec08f0cdd76d626056fd10
EricLScace <mail@ericscace.com>
2017-06-28 13:44:01 -0400

    Build route & controller to trap datatable message
    What:
    * Router for '/bucket', based on user, to route the GET message.
    * Controller, based on example, that has a debugger breakpoint at
    every function.
    
    Why: controller should breakpoint whenever a message from datatables
    arrives so we can inspect the message contents and decide how to
    read/write the data object into MongoDB.
    
    References: #24 #20 #21

--- config/routes.js
+++ config/routes.js
@@ -15,6 +15,5 @@ module.exports = require('lib/wiring/routes')
 .post('/sign-in', 'users#signin')
 .delete('/sign-out/:id', 'users#signout')
 .patch('/change-password/:id', 'users#changepw')
-// .resources('users', { only: ['index', 'show'] })
 
-// all routes created
+.get('/bucket', 'buckets#index')
